58cc62dc1e66fa766de949c7fe6d1b2ef68e9cf5
Dorian <vdoralex@gmail.com>
2021-09-07 19:45:42 +0200

    YT download works

--- app/src/main/java/dorian/fr/universaldownloader/MainActivity.java
+++ app/src/main/java/dorian/fr/universaldownloader/MainActivity.java
@@ -2,13 +2,91 @@ package dorian.fr.universaldownloader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import at.huber.youtubeExtractor.YouTubeExtractor;
+import at.huber.youtubeExtractor.YouTubeUriExtractor;
+import at.huber.youtubeExtractor.YtFile;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button downloadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.recieved);
+        getTextFromShare();
+
+        downloadButton = findViewById(R.id.downloadButton);
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                DownloadYTVideo();
+
+            }
+        });
+
+
+    }
+
+
+    private void getTextFromShare() {
+
+        Intent intent = getIntent();
+
+
+        String sharedText = intent.getExtras().getString(Intent.EXTRA_TEXT);
+
+        if (sharedText != null) {
+            textView.setText(sharedText);
+        }
+
+    }
+
+    private void DownloadYTVideo() {
+        String values = textView.getText().toString();
+        YouTubeUriExtractor youTubeUriExtractor = new YouTubeUriExtractor(MainActivity.this) {
+            @Override
+            public void onUrisAvailable(String videoId, String videoTitle, SparseArray<YtFile> ytFiles) {
+                if (ytFiles != null) {
+                    int tag = 22;
+                    String title = values.replace("https://youtu.be/", "");
+                    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(ytFiles.get(tag).getUrl()));
+                    request.setTitle(title);
+                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, title + ".mp4");
+                    DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                    request.allowScanningByMediaScanner();
+                    request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
+                    downloadManager.enqueue(request);
+                }
+            }
+        };
+        youTubeUriExtractor.execute(values);
     }
 }
